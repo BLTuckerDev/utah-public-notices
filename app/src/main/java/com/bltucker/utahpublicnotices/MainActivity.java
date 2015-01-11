7c3c75ee0b4d85518e5972dec53a8594a35a0f44
Brett Tucker <bretttucker@gmail.com>
2015-01-11 16:29:00 -0700

    Saves a todo for later.

--- app/src/main/java/com/bltucker/utahpublicnotices/MainActivity.java
+++ app/src/main/java/com/bltucker/utahpublicnotices/MainActivity.java
@@ -52,7 +52,7 @@ public final class MainActivity extends Activity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return true;//TODO move the settings option to the notice list fragment.
     }
 
 
