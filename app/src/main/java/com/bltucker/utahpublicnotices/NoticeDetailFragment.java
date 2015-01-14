58a4a86c1e5a56dbaf7a4a55c601537e14328831
Brett Tucker <bretttucker@gmail.com>
2015-01-13 19:58:13 -0700

    Adds a share provider so that later I can easily add the ability to share meeting details.

--- app/src/main/java/com/bltucker/utahpublicnotices/NoticeDetailFragment.java
+++ app/src/main/java/com/bltucker/utahpublicnotices/NoticeDetailFragment.java
@@ -1,17 +1,43 @@
 package com.bltucker.utahpublicnotices;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ShareActionProvider;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class NoticeDetailFragment extends Fragment {
+public final class NoticeDetailFragment extends Fragment {
 
     public NoticeDetailFragment() {
+        this.setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_fragment_details, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        ShareActionProvider shareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        if(shareActionProvider != null){
+            shareActionProvider.setShareIntent(getShareIntent());
+        }
+    }
+
+
+    private Intent getShareIntent() {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "You Shared!");
+
+        return shareIntent;
     }
 
 
