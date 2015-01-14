2eaffa4e5f30abc1df415f61469f4bba88c2fb60
Brett Tucker <bretttucker@gmail.com>
2015-01-13 20:26:28 -0700

    Adding an intent to show where the meeting is at. Adds icons for the meeting share action.

--- app/src/main/java/com/bltucker/utahpublicnotices/NoticeDetailFragment.java
+++ app/src/main/java/com/bltucker/utahpublicnotices/NoticeDetailFragment.java
@@ -2,6 +2,7 @@ package com.bltucker.utahpublicnotices;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -31,10 +32,34 @@ public final class NoticeDetailFragment extends Fragment {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_map:
+                showMeetingOnMap();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void showMeetingOnMap() {
+        Intent mapIntent = new Intent();
+        mapIntent.setAction(Intent.ACTION_VIEW);
+        //TODO replace with actual meeting location!
+        String meetingAddress = "10 North Main, Cedar City,  84720";
+        mapIntent.setData(Uri.parse(String.format("geo:0,0?q=%s", meetingAddress)));
+        startActivity(mapIntent);
+    }
+
+
     private Intent getShareIntent() {
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
+        //TODO: replace with actual meeting details for the share!
         shareIntent.putExtra(Intent.EXTRA_TEXT, "You Shared!");
 
         return shareIntent;
